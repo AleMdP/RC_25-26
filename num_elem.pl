dc63130154c772f4a67680d6d262a988590a8e72
Alejandro Macías del Pozo <maciasdelpozoalejandro@gmail.com>
2026-02-25 11:26:33 +0100

    Add files via upload

---
+++ num_elem.pl
@@ -0,0 +1,13 @@
+/*
+num_elem(+Lista, -Resultado)
+	es cierto si Resultado unifica con
+	el número de elementos de Lista.
+	
+Principio de inducción
+	1) num_elem([], ).
+	2) num_elem(n-1) -> num_elem(n).
+	
+*/
+
+num_elem([], 0).
+num_elem(n) :- num_elem(n-1).
