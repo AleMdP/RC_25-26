f18d6be8b6c93de0534715912aad4516ea8346b0
Alejandro Macías del Pozo <maciasdelpozoalejandro@gmail.com>
2026-03-26 09:17:17 +0100

    Add files via upload

---
+++ ordenada.pl
@@ -0,0 +1,8 @@
+/*
+ordenada(+Lista)
+	es cierto si Lista está ordenada de menor a mayor.
+*/
+
+ordenada([]).
+ordenada([_]).
+ordenada([Cab1,Cab2|Resto]):- Cab1 =< Cab2, ordenada([Cab2|Resto]).
---
+++ primos_entre_x_y.pl
@@ -0,0 +1,44 @@
+/*
+lista_divisores(+X, +Y, -ListaR).
+	Es cierto cuando ListaR unifica con una lista que contiene a los números
+	cuyo resto de la división entera de X entre Z es igual a 0 para valores 
+	de Z entre 1 e Y.
+*/
+
+lista_divisores(_, 1, [1]).
+lista_divisores(X, Y, [Y|Resto]):-  Y > 1,
+									0 is X mod Y, 
+									Y2 is Y - 1, 
+									lista_divisores(X, Y2, Resto).
+lista_divisores(X, Y, LR):- \+ 0 is X mod Y, 
+							Y2 is Y - 1, 
+							lista_divisores(X, Y2, LR).
+
+%--------------------------------------------------------------------------------------
+
+/*
+primo(+X).
+	Es cierto si X unifica con un número primo.
+*/
+
+primo(X):- lista_divisores(X, X, [X, 1]).
+
+%--------------------------------------------------------------------------------------
+
+/*
+primosEntrexy(+X, +Y, -ListaR).
+	Es cierto cuando ListaR unifica con una lista que contiene a los primos que van 
+	desde X hasta Y ambos incluidos en orden ascendente.
+*/
+
+primosEntrexy(X, X, [X]):- primo(X).
+primosEntrexy(X, X, []):- \+ primo(X).
+primosEntrexy(X, Y, [X|R]):- X < Y, 
+						X2 is X + 1, 
+						primo(X), 
+						primosEntrexy(X2, Y, R).
+primosEntrexy(X, Y, R):- X < Y, 
+						X2 is X + 1, 
+						\+ primo(X), 
+						primosEntrexy(X2, Y, R).
+						
