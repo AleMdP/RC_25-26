38e9c980cffc85edcf5cf9c53f837626a6f16f33
Alejandro Macías del Pozo <maciasdelpozoalejandro@gmail.com>
2026-05-14 09:50:03 +0200

    Provisional

---
+++ timetables/mi_timetable.pl
@@ -0,0 +1,57 @@
+
+:- load_files('req2.pl').
+
+:- use_module(library(clpfd)).
+/*
+pairs_keys([a-1,b-2,c-3], Keys).
+	Keys = [a, b, c].
+
+pairs_values([a-1,b-2,c-3], R).
+	R = [1, 2, 3].
+*/
+
+requirements(Rs):- Goal = class_subject_teacher_times(Class, Subject, Teacher, Number),
+				   setof(req(Class, Subject, Teacher, Number), Goal, Rs0),
+				   maplist(req_with_slots, Rs0, Rs).
+
+req_with_slots(R, R-Slots):- R = req(_, _, _, N), length(Slots, N).
+
+
+classes(Classes):- setof(C, S^N^T^class_subject_teacher_times(C,S,T,N), Classes).
+
+teachers(Teachers):- setof(T, C^S^N^class_subject_teacher_times(C,S,T,N), Teachers).
+
+requirements_variables(Rs, Vars):- requirements(Rs),
+								   pairs_slots(Rs, Vars),
+								   slots_per_week(SPW),
+								   Max #= SPW - 1,
+								   Vars ins 0..Max,
+								   maplist(constrain_subject, Rs),
+								   classes(Classes),
+								   teachers(Teachers),
+								   rooms(Rooms),
+								   maplist(constrain_teacher(Rs), Teachers),
+								   maplist(constrain_class(Rs), Classes),
+								   maplist(constrain_rooms(Rs), Rooms),
+
+pairs_slots(Ps, Vs):- pairs_values(Ps, Vs0),
+					  append(Vs0, Vs).
+
+constrain_subject(req(Class, Subj, _Teacher, _Num)-Slots):- strictly_ascending(Slots),
+															maplist(slot_quotient, Slots, Qs0),
+															findall(F-S, coupling(Class, Subj, F, S), Cs),
+															maplist(slots_couplings(Slots), Cs),
+															pairs_values(Cs, Seconds0),
+															sort(Seconds0, Seconds),
+															list_without_nths(Qs0, Seconds, Qs),
+															strictly_ascending(Qs).
+
+strictly_ascending(Ls):- chain(Ls, #<).
+/*
+chain([A,B,C], #<).
+	B#=<C+ -1,
+	A#=<B+ -1.
+*/
+
+slot_quotient(S, Q):- slots_per_day(SPD),
+					  Q #= S // SPD.
