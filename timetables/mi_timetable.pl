539e19f8cfd93524b623604598213f26bbbdab5e
Alejandro Macías del Pozo <maciasdelpozoalejandro@gmail.com>
2026-05-28 09:52:42 +0200

    Update mi_timetable.pl

--- timetables/mi_timetable.pl
+++ timetables/mi_timetable.pl
@@ -26,19 +26,33 @@ classes(Classes):- setof(C, S^N^T^class_subject_teacher_times(C,S,T,N), Classes)
 
 teachers(Teachers):- setof(T, C^S^N^class_subject_teacher_times(C,S,T,N), Teachers).
 
+rooms(Rooms):- findall(Room, room_alloc(Room,_C,_S,_Slot), Rooms0),
+			   sort(Rooms0, Rooms).
+
 requirements_variables(Rs, Vars):- requirements(Rs),
 								   pairs_slots(Rs, Vars),
 								   slots_per_week(SPW),
 								   Max #= SPW - 1,
 								   Vars ins 0..Max,
-								   maplist(constrain_subject, Rs).
+								   maplist(constrain_subject, Rs),
+								   classes(Classes),
+								   teachers(Teachers),
+								   rooms(Rooms),
+								   maplist(constrain_teacher(Rs), Teachers),
+								   maplist(constrain_class(Rs), Classes),
+								   maplist(constrain_room(Rs), Rooms).
 
 pairs_slots(Ps, Vs):- pairs_values(Ps, Vs0),
 					  append(Vs0, Vs).
 
 constrain_subject(req(Class, Subj, _Teacher, _Num)-Slots):- strictly_ascending(Slots), % break symmetry
 															maplist(slot_quotient, Slots, Qs0),
-															strictly_ascending(Qs0).
+															%findall(F-S, coupling(Class, Subj, F, S), Cs),
+															%maplist(slots_coupling(Slots), Cs),
+															%pairs_values(Cs, Seconds0),
+															%sort(Seconds0, Seconds),
+															%list_without_nths(Qs0, Seconds, Qs),
+															strictly_ascending(Qs).
 
 strictly_ascending(Ls):- chain(Ls, #<).
 /*
@@ -52,7 +66,10 @@ slot_quotient(S, Q):- slots_per_day(SPD),
 
 constrain_teacher(Rs, Teacher):- mi_tfilter(Teacher, Rs, Sub),
 								 pairs_slots(Sub, Vs),
-								 all_different(Vs).
+								 all_different(Vs),
+								 findall(F, teacher_freeday(Teacher, F), Fs),
+								 maplist(slot_quotient, Vs, Qs),
+								 maplist(all_dif_from(Qs), Fs).
 
 teacher_req(T0, req(_C, _S, T1, _N)-_, T):- =(T0, T1, T).
 
